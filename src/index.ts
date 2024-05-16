a29d602e281475bb0f4cf86b05a8b6396fb93d1d
GuilhermeLimaSoares <gsoares@guilhermePc>
2024-05-15 22:33:41 -0300

    feat/connection to bank

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { AppDataSource } from './data-source'
 import routes from './routes'
+import { Request, Response } from "express"
 
 AppDataSource.initialize().then(() => {
 	const app = express()
@@ -8,6 +9,11 @@ AppDataSource.initialize().then(() => {
 	app.use(express.json())
 
 	app.use(routes)
+
+//    app.get("/users", (req: Request, res: Response) => {
+//       // here we will have logic to return all users
+//       return res.json('ok, deu certo!')
+//   })
 
 	return app.listen(process.env.PORT)
 })
