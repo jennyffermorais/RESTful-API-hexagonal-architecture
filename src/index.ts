e0ec583fc1cba781f139b47b2180e17cc2f3aead
jennyffermorais <jennyffernovaes@hotmail.com>
2024-05-26 17:35:48 -0300

    fix database connection issue inside container

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 import 'reflect-metadata';
 import { AppDataSource } from '../data-source'; // ajuste o caminho conforme necessário
 import express from 'express';
+
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as logger from 'morgan';
+
 import productRoute from './routers/productRoute';
 import clientRoute from './routers/clientRoute';
 
 const app = express();
-
-app.use(express.json(), clientRoute, productRoute)
+app.use(cors.default());
+app.use(bodyParser.json());
+app.use(logger.default('dev'));
 
 AppDataSource.initialize()
    .then(() => {
       app.listen(process.env.APP_PORT || 3000, () => {
          console.log(
-            `Server is running on port ${process.env.APP_PORT || 3000}`
+            `Server is running on port ${process.env.APP_} ${process.env.APP_PORT || 3000}`
          );
       });
    })
    .catch((error) => console.log(error));
+
+app.use(express.json(), clientRoute, productRoute);
