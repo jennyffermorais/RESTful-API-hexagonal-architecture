6ae705126b7f05b06e6691693b59a60f2108f18f
Paulo Pupo <devpupo@outlook.com>
2024-05-27 01:41:54 -0300

    feat: add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import 'reflect-metadata';
 import express from 'express';
 import { AppDataSource } from './data-source';
-
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as logger from 'morgan';
-
 import clientRoute from './routers/clientRoute';
 import categoryRoute from "./routers/categoryRoute";
 import productRoute from './routers/productRoute';
+import orderRoute from './routers/orderRoute';
 import { paymentRoute } from './routers/paymentRoute';
+import swaggerUi from "swagger-ui-express";
+import path from 'path';
+import swaggerDocument from "../dist/swagger.json"
 
 const app = express();
 
@@ -18,8 +20,9 @@ AppDataSource.initialize()
     app.use(cors.default());
     app.use(bodyParser.json());
     app.use(logger.default('dev'));
-    app.use(express.json(), categoryRoute, clientRoute, productRoute, paymentRoute);
-    app.use('/', (req, res) => res.send('restfull-api-hexagonal-architecture ok'));
+    app.use(express.json(), categoryRoute, clientRoute, productRoute, paymentRoute, orderRoute);
+
+    app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
     app.listen(process.env.APP_PORT || 3000, () => {
       console.log(`Server is running on port ${process.env.APP_PORT || 3000}`);
